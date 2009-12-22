eaabaf21f5e2820a2908505dff1b42ce65b71e01
arfrank <arfrank@gmail.com>
2009-12-21 22:37:37 -0500

    Adding jQuery

--- system/application/views/global/top.php
+++ system/application/views/global/top.php
@@ -2,6 +2,10 @@
 <head>
 	<title><?php echo $title; ?></title>
 	<link rel="stylesheet" href="master.css" type="text/css" media="screen" title="no title" charset="utf-8">
+	<script src="http://www.google.com/jsapi"></script>
+	<script type="text/javascript">
+		google.load("jquery", "1.3.2");
+	</script>
 </head>
 <body>
 <div id="top_bar">
